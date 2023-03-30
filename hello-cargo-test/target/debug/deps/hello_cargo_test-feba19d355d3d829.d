f331584ff45414888833b4b2e85cd601bad2d3b4
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-30 22:53:31 +0300

    created and run a simple rust program using cargo

---
+++ hello-cargo-test/target/debug/deps/hello_cargo_test-feba19d355d3d829.d
@@ -0,0 +1,5 @@
+/Users/bizee/Documents/Code_Projects/Rust-By-Practice/hello-cargo-test/target/debug/deps/hello_cargo_test-feba19d355d3d829: src/main.rs
+
+/Users/bizee/Documents/Code_Projects/Rust-By-Practice/hello-cargo-test/target/debug/deps/hello_cargo_test-feba19d355d3d829.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo-test/target/debug/hello-cargo-test.d
@@ -0,0 +1,1 @@
+/Users/bizee/Documents/Code_Projects/Rust-By-Practice/hello-cargo-test/target/debug/hello-cargo-test: /Users/bizee/Documents/Code_Projects/Rust-By-Practice/hello-cargo-test/src/main.rs
